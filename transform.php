1d5019d0dbd19f15fd236a3efd4d00c48bc2de79
Chris McKenzie <kristopolous@yahoo.com>
2013-04-20 22:31:19 -0700

    fixed a typo

--- transform.php
+++ transform.php
@@ -7,7 +7,7 @@ $host = $pieces['host'];
 
 // remove annoying spurious www
 $host = preg_replace('/^www\./', '', $host);
-j
+
 // and a .com while you are at it
 $host = preg_replace('/\.com$/', '', $host);
 
