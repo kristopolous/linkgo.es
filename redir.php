d1ff75b9b409bff54dc05c085510f613d0f24447
Chris McKenzie <kristopolous@yahoo.com>
2013-04-25 02:41:39 -0700

    real black magic

--- redir.php
+++ redir.php
@@ -9,6 +9,8 @@ if (
 ) {
   $type = $parts[0];
   require('transform.php');
+} else if(!empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST'])) {
+  echo file_get_contents("index.html");
 } else if($redir = get($_GET['url'])) {
   header("Location: $redir", TRUE, 302);
 } else {
